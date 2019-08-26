2bbb3c73dff065d4ce69a2d7852cf4dfab655d0b
Olga <rizraelleshinobu@gmail.com>
2019-08-26 20:43:15 +0300

    🚀

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true,
-  walletUrl: '{WALLET_URL}',
-  votingUrl: '{VOTING_URL}',
-  appName: '{APP_NAME}',
-  logoUrl: '{LOGO_URL}',
-  blockchainUrl: '{BLOCKCHAIN_URL}',
-  chainId: '{CHAIN_ID}',
-  showAds: {SHOW_ADS},
-  tokensUrl: '{TOKENS_URL}',
-  tickerUrl: '{TICKER_URL}',
-  token: '{TOKEN}'
+  production: false,
+  walletUrl: 'https://walleteos.com',
+  votingUrl: 'https://eosportal.io',
+  appName: 'EOS Tracker',
+  logoUrl: '/assets/logo.png',
+  blockchainUrl: 'https://proxy.eosnode.tools',
+  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+  showAds: true,
+  tokensUrl: 'https://raw.githubusercontent.com/eoscafe/eos-airdrops/master/tokens.json',
+  tickerUrl: 'https://api.coinmarketcap.com/v2/ticker/1765/',
+  token: 'EOS'
 };
